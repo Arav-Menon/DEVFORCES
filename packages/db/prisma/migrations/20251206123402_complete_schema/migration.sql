55d1d7228358bfd877b3319ecfd3f6cd9045f14e
Arav menon <aravmenon.ak@gmail.com>
2025-12-06 19:08:33 +0530

    add schema migration to db

---
+++ packages/db/prisma/migrations/20251206123402_complete_schema/migration.sql
@@ -0,0 +1,91 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `role` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'user');
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "role" "Role" NOT NULL,
+ADD COLUMN     "username" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Contest" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "contestToChallengeMappingId" TEXT,
+
+    CONSTRAINT "Contest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ContestToChallengeMapping" (
+    "id" TEXT NOT NULL,
+    "contestId" TEXT NOT NULL,
+    "challengeId" TEXT NOT NULL,
+    "index" INTEGER NOT NULL,
+
+    CONSTRAINT "ContestToChallengeMapping_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Challenge" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "notionDocId" TEXT NOT NULL,
+    "maxPoints" INTEGER NOT NULL,
+    "contestToChallengeMappingId" TEXT,
+
+    CONSTRAINT "Challenge_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" TEXT NOT NULL,
+    "points" INTEGER NOT NULL,
+    "submittedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "contestToChallengeMappingId" TEXT,
+    "userId" TEXT,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Leaderboard" (
+    "id" TEXT NOT NULL,
+    "rank" INTEGER NOT NULL,
+    "userId" TEXT,
+    "contestId" TEXT,
+
+    CONSTRAINT "Leaderboard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ContestToChallengeMapping_contestId_challengeId_key" ON "ContestToChallengeMapping"("contestId", "challengeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Leaderboard_rank_contestId_key" ON "Leaderboard"("rank", "contestId");
+
+-- AddForeignKey
+ALTER TABLE "Contest" ADD CONSTRAINT "Contest_contestToChallengeMappingId_fkey" FOREIGN KEY ("contestToChallengeMappingId") REFERENCES "ContestToChallengeMapping"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Challenge" ADD CONSTRAINT "Challenge_contestToChallengeMappingId_fkey" FOREIGN KEY ("contestToChallengeMappingId") REFERENCES "ContestToChallengeMapping"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_contestToChallengeMappingId_fkey" FOREIGN KEY ("contestToChallengeMappingId") REFERENCES "ContestToChallengeMapping"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Leaderboard" ADD CONSTRAINT "Leaderboard_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Leaderboard" ADD CONSTRAINT "Leaderboard_contestId_fkey" FOREIGN KEY ("contestId") REFERENCES "Contest"("id") ON DELETE SET NULL ON UPDATE CASCADE;
