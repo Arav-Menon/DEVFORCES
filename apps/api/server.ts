b808b609c75949da92bb3db79e245287a38a4f0b
Arav menon <aravmenon.ak@gmail.com>
2025-12-09 20:23:59 +0530

    setup express routes

---
+++ apps/api/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+import { authRoute } from "./routes/user/auth.user.js";
+import { deleteUserRoute } from "./routes/user/delete.user.js";
+import { updateUserRoute } from "./routes/user/update.user.js";
+import { userRoute } from "./routes/user/user.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/v1/user/", authRoute);
+app.use("/v1/user/", deleteUserRoute);
+app.use("/v1/user/", updateUserRoute);
+app.use("/v1/user/", userRoute);
+
+app.listen(4000);
