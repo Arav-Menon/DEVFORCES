ced901372bc4fcfd45eda41f6c2294c316de4918
Arav menon <aravmenon.ak@gmail.com>
2025-12-12 06:29:25 +0530

    write get end point for getting user profile

--- apps/api/routes/user/user.ts
+++ apps/api/routes/user/user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import type { Request, Response } from "express";
+import { db } from "@repo/db/db";
+import { auth } from "../middleware/auth";
 
 export const userRoute = express.Router();
 
-userRoute.get("/auth", (_, res) => {
-  res.json({
-    message: "hitting to user auth route",
-  });
+userRoute.get("/profile/:userId", auth, async (req: Request, res: Response) => {
+  try {
+    const userId = req.id;
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: { username: true, email: true },
+    });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 });
