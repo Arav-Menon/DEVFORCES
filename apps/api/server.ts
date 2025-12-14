3edb3b5efc94a7e472b1033f44faa5c05739f663
Arav menon <aravmenon.ak@gmail.com>
2025-12-14 09:25:07 +0530

    add new routes of challenges and contests

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,17 +1,31 @@
 import express from "express";
 import "dotenv/config";
-import { authRoute } from "./routes/user/auth.user.js";
-import { deleteUserRoute } from "./routes/user/delete.user.js";
-import { updateUserRoute } from "./routes/user/update.user.js";
-import { userRoute } from "./routes/user/user.js";
+import { authRoute } from "./routes/user/auth.user";
+import { deleteUserRoute } from "./routes/user/delete.user";
+import { updateUserRoute } from "./routes/user/update.user";
+import { userRoute } from "./routes/user/user";
+import { contestRouter } from "./routes/contest/contests";
+import { updateContestRouter } from "./routes/contest/update.contests";
+import { deleteContestRouter } from "./routes/contest/delete.contest";
+import { challengeRouter } from "./routes/challenge/challenge";
+import { deleteChallengeRouter } from "./routes/challenge/delete.challenge";
+import { updateChallengeRouter } from "./routes/challenge/update.challenge";
 
 const app = express();
 app.use(express.json());
 
-app.use("/v1/user/", authRoute);
-app.use("/v1/user/", deleteUserRoute);
-app.use("/v1/user/", updateUserRoute);
-app.use("/v1/user/", userRoute);
+app.use("/api/v1/user/", authRoute);
+app.use("/api/v1/user/", userRoute);
+app.use("/api/v1/user/", updateUserRoute);
+app.use("/api/v1/user/", deleteUserRoute);
+
+app.use("/api/v1/contest/", contestRouter);
+app.use("/api/v1/contest/", updateContestRouter);
+app.use("/api/v1/contest/", deleteContestRouter);
+
+app.use("/api/v1/contest/", challengeRouter);
+app.use("/api/v1/contest/", updateChallengeRouter);
+app.use("/api/v1/contest/", deleteChallengeRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at PORT ${process.env.PORT}`);
