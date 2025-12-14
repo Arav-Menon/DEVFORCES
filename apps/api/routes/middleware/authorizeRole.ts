bde8e776305b424e884960b24d6fcf3e77b1c1eb
Arav menon <aravmenon.ak@gmail.com>
2025-12-14 09:21:56 +0530

    Add strict RBAC control

--- apps/api/routes/middleware/authorizeRole.ts
+++ apps/api/routes/middleware/authorizeRole.ts
@@ -1,12 +1,14 @@
 import type { Request, Response, NextFunction } from "express";
 
-export const authorizeRole = (...allowRoles: any) => {
+export const authorizeRole = (allowedRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     //@ts-ignore
-    const userRole = req.user.role;
-
-    if (!allowRoles.includes(userRole)) {
-      return res.status(403).json({ message: "Access denied" });
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthenticated", Error: Error });
+    }
+    //@ts-ignore
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Acces denied", Error: Error });
     }
 
     next();
