eaeb86f64ca790f2a93b50ac28e6ceb29459f4af
Arav menon <aravmenon.ak@gmail.com>
2025-12-14 09:17:33 +0530

    write contest and challenge zod validation schema

--- packages/common/validation.ts
+++ packages/common/validation.ts
@@ -13,3 +13,37 @@ export const userSchema = z.object({
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[0-9]/, "Password must contain at least one number"),
 });
+
+export const contestSchema = z.object({
+  title: z.string().trim().min(5).max(100),
+  startTime: z.coerce.date().refine((d) => d.getTime() > Date.now(), {
+    message: "Start time must be in the future",
+  }),
+});
+
+export const challengeSchema = z.object({
+  title: z.string().trim().min(5).max(100),
+  notionDocId: z
+    .string()
+    .url()
+    .refine(
+      (url) => {
+        try {
+          const parsed = new URL(url);
+          if (
+            parsed.hostname !== "notion.so" &&
+            parsed.hostname !== "www.notion.so"
+          ) {
+            return false;
+          }
+          return /[a-f0-9]{32}/i.test(parsed.pathname);
+        } catch {
+          return false;
+        }
+      },
+      {
+        message: "Invalid Notion document link",
+      }
+    ),
+  maxPoint: z.number().min(10).max(100),
+});
