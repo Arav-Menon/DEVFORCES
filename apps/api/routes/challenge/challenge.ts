09ddece0254bf69d58d13ef40911987d3d2ef0fc
Arav menon <aravmenon.ak@gmail.com>
2025-12-14 09:23:30 +0530

    complete creating challenge endpoint for contest

---
+++ apps/api/routes/challenge/challenge.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import { challengeSchema } from "@repo/common/validation";
+import { db } from "@repo/db/db";
+import { middleware } from "../middleware/auth";
+import { authorizeRole } from "../middleware/authorizeRole";
+
+export const challengeRouter = express.Router();
+
+challengeRouter.post(
+  "/:contestId/challenges",
+  middleware,
+  authorizeRole(["ADMIN", "CREATOR"]),
+  async (req, res) => {
+    const contestId = req.params.contestId;
+    const body = req.body;
+    const result = challengeSchema.safeParse(body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        error: result.error.flatten().fieldErrors,
+      });
+    }
+
+    try {
+      const { title, notionDocId, maxPoint } = result.data;
+
+      const findChallenge = await db.challenge.findUnique({
+        where: {
+          title,
+        },
+      });
+
+      if (findChallenge) {
+        return res.status(401).json({
+          message: `Challenge with this title ${title} already exist`,
+        });
+      }
+
+      const createChallenge = await db.challenge.create({
+        data: {
+          title: title,
+          notionDocId: notionDocId,
+          maxPoints: maxPoint,
+          contestId: contestId,
+        },
+      });
+
+      res.status(201).json({
+        message: "challenge has been created",
+        Challenge: {
+          title: createChallenge.title,
+          notionDocId: createChallenge.notionDocId,
+          maxPoint: createChallenge.maxPoints,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  }
+);
