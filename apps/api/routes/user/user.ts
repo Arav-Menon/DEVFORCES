1cdfb4095ea29842647e8da8f4777def96692f39
Arav menon <aravmenon.ak@gmail.com>
2025-12-14 09:20:53 +0530

    change middleware name

--- apps/api/routes/user/user.ts
+++ apps/api/routes/user/user.ts
@@ -1,21 +1,25 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { db } from "@repo/db/db";
-import { auth } from "../middleware/auth";
+import { middleware } from "../middleware/auth";
 
 export const userRoute = express.Router();
 
-userRoute.get("/profile/:userId", auth, async (req: Request, res: Response) => {
-  try {
-    const userId = req.id;
-    const user = await db.user.findUnique({
-      where: { id: userId },
-      select: { username: true, email: true },
-    });
+userRoute.get(
+  "/profile/:userId",
+  middleware,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = req.id;
+      const user = await db.user.findUnique({
+        where: { id: userId },
+        select: { username: true, email: true },
+      });
 
-    if (!user) return res.status(404).json({ message: "User not found" });
-    return res.status(200).json({ user });
-  } catch (error) {
-    return res.status(500).json({ error });
+      if (!user) return res.status(404).json({ message: "User not found" });
+      return res.status(200).json({ user });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   }
-});
+);
