0acd9235ee0557abdf78308f13f7a4218dd2736f
Arav menon <aravmenon.ak@gmail.com>
2025-12-19 02:58:26 +0530

    add submit challenge file

---
+++ apps/api/routes/lab/submit-challenge/submit.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+export const submitRouter = express.Router();
+
+submitRouter.post("/submit/:challengeId ", (req, res) => {
+  res.json({
+    message: "working",
+  });
+});
