6a3b68c5911963a8cbfc26800bbab189d57bdc34
Arav menon <aravmenon.ak@gmail.com>
2025-12-20 01:52:30 +0530

    remove placeholder response from submit route

--- apps/api/routes/lab/submit-challenge/submit.ts
+++ apps/api/routes/lab/submit-challenge/submit.ts
@@ -3,7 +3,5 @@ import express from "express";
 export const submitRouter = express.Router();
 
 submitRouter.post("/submit/:challengeId ", (req, res) => {
-  res.json({
-    message: "working",
-  });
+  
 });
