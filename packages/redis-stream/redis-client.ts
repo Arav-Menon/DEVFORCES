1c6a27a077f7ef4b18d14d06785666ef682ed8c5
Arav menon <aravmenon.ak@gmail.com>
2025-12-25 08:26:06 +0530

    feat: add pushSubmission function to handle submission events

--- packages/redis-stream/redis-client.ts
+++ packages/redis-stream/redis-client.ts
@@ -1,5 +1,33 @@
 import { createClient } from "redis";
+import "dotenv/config";
 
 export const client = await createClient()
   .on("error", (err) => console.log("Redis client error", err))
   .connect();
+
+type SubmissionEvent = {
+  submissionId: string;
+  userId: string;
+  challengeId: any;
+  code: string;
+  language: string;
+  systemPrompt: string;
+};
+
+export const pushSubmission = async ({
+  submissionId,
+  userId,
+  challengeId,
+  code,
+  language,
+  systemPrompt,
+}: SubmissionEvent) => {
+  await client.xAdd("submisson:stream", "*", {
+    submissionId,
+    userId,
+    challengeId,
+    code,
+    language,
+    systemPrompt,
+  });
+};
