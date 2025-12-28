82f4add0d0c144044be623155f9fb18ab55f8c47
Arav menon <aravmenon.ak@gmail.com>
2025-12-28 16:18:24 +0530

    setup worker for reading queue messages and send for ai response

--- apps/worker/worker.ts
+++ apps/worker/worker.ts
@@ -1,9 +1,25 @@
 import { pullSubmission } from "@repo/redis-stream/redis-client";
+import { processWithAi } from "@repo/ai-evaluator/evaluator";
 
-while(true) {
-    const submissions = await pullSubmission()
+while (true) {
+  console.log("worker waiting for job");
 
-    // if(submissions) {
-    //     await processWithAi(submissions)
-    // }
+  const submissions = (await pullSubmission()) as any;
+  if (!submissions) continue;
+
+  console.dir(submissions, { depth: null });
+
+  const stream = submissions[0];
+  const record = stream?.messages[0];
+
+  const { systemPrompt, code, challengeId, userId } = record.message;
+
+  console.log("PROCESSING:", record.id);
+
+  await processWithAi({
+    systemPrompt,
+    code,
+    challengeId,
+    userId,
+  });
 }
