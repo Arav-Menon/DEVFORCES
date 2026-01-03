3755551ad0ccc21f6391fc859aff672f42e4fadf
Arav menon <aravmenon.ak@gmail.com>
2026-01-03 19:13:19 +0530

    setup realtime server for subs client

---
+++ apps/realtime/server.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws";
+import { clients } from "./broadcast/wsClients";
+import { startNotificationSubscriber } from "./broadcast/subscriber";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws: any) => {
+  clients.add(ws);
+
+  ws.on("close", () => {
+    clients.delete(ws);
+  });
+});
+
+startNotificationSubscriber();
