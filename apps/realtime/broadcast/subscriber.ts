f1ce9d7748b536d23dd80a31a03876d0480a7113
Arav menon <aravmenon.ak@gmail.com>
2026-01-04 22:57:03 +0530

    Add redis client for storing parsed data

--- apps/realtime/broadcast/subscriber.ts
+++ apps/realtime/broadcast/subscriber.ts
@@ -1,14 +1,19 @@
 import Redis from "ioredis";
 import { broadcast } from "./broadcast";
+import { client } from "@repo/redis-stream/redis-client";
+import { v4 as uuidV4 } from "uuid";
 
 const subClient = new Redis();
+const evaluationId = uuidV4();
 
-export const startNotificationSubscriber = async () => {
+export const startNotificationSubscriber = () => {
   const response = subClient.subscribe("submission:notification");
   console.log(response);
-  subClient.on("message", (_channel, message) => {
+  subClient.on("message", async (_channel, message) => {
     const parsed = JSON.parse(message);
-    console.log(parsed)
+    await client.set(`evaluation${evaluationId}`, JSON.stringify(parsed), {
+      EX: 3600,
+    });
     broadcast(parsed);
   });
 };
