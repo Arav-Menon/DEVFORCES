d3206f034af349ebfaba17ea242ce4a48ccd9e4b
Arav menon <aravmenon.ak@gmail.com>
2026-01-06 23:19:07 +0530

    setup leaderboard router

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -14,6 +14,7 @@ import { aiModelRouter } from "./routes/model/model";
 import { contestsRouter } from "./routes/lab/contests";
 import { challengesRouter } from "./routes/lab/challenges";
 import { submitRouter } from "./routes/lab/submit-challenge/submit";
+import { leaderboardRouter } from "./routes/leaderboard/leaderboard";
 
 const app = express();
 app.use(express.json());
@@ -37,6 +38,8 @@ app.use("/api/v1/", contestsRouter);
 app.use("/api/v1/contest/", challengesRouter);
 
 app.use("/api/v1/challenge/", submitRouter);
+
+app.use("/api/v1/leaderboard", leaderboardRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at PORT ${process.env.PORT}`);
