b4467ff0263d0d26bda362d3b6a405fa4fc83e85
Arav menon <aravmenon.ak@gmail.com>
2026-01-07 23:53:46 +0530

    add contestId route and set contestId function

--- apps/api/routes/lab/submit-challenge/submit.ts
+++ apps/api/routes/lab/submit-challenge/submit.ts
@@ -1,46 +1,54 @@
 import express from "express";
 import { getSystemPrompt } from "../../prompt";
-import { getChallengeById } from "./fetchChallenge";
+import { getChallengeById } from "./fetchId";
 import { v4 as uuidV4 } from "uuid";
 import { middleware } from "../../middleware/auth";
 import { client, pushSubmission } from "@repo/redis-stream/redis-client";
+import { getContestById } from "./fetchId";
 
 export const submitRouter = express.Router();
 
-submitRouter.post("/submit/:challengeId", middleware, async (req, res) => {
-  const userId = req.id;
-  const challengeId = req.params.challengeId;
-  const { code, language } = req.body;
+submitRouter.post(
+  "/submit/:contestId/:challengeId",
+  middleware,
+  async (req, res) => {
+    const userId = req.id;
+    const challengeId = req.params.challengeId;
+    const contestId = req.params.contestId;
+    const { code, language } = req.body;
 
-  if (!challengeId || !code || !language || !userId) {
-    return res.status(400).json({
-      message: "Invalid submission",
+    if (!challengeId || !code || !language || !userId || !contestId) {
+      return res.status(400).json({
+        message: "Invalid submission",
+      });
+    }
+
+    const submissionId = uuidV4();
+    const challenge = await getChallengeById(challengeId);
+    await getContestById(contestId);
+    const systemPrompt = getSystemPrompt(challenge);
+
+    const payload = {
+      submissionId,
+      contestId,
+      userId: userId as string,
+      challengeId,
+      code,
+      language,
+      systemPrompt,
+    };
+
+    await client.set(`submission:${submissionId}`, JSON.stringify(payload), {
+      EX: 3600,
+    });
+
+    const submissionResult = await pushSubmission(payload);
+
+    console.log(submissionResult);
+
+    res.status(200).json({
+      submissionId,
+      message: "Submission received",
     });
   }
-
-  const submissionId = uuidV4();
-  const challenge = await getChallengeById(challengeId);
-  const systemPrompt = getSystemPrompt(challenge);
-
-  const payload = {
-    submissionId,
-    userId: userId as string,
-    challengeId,
-    code,
-    language,
-    systemPrompt,
-  };
-
-  await client.set(`submission:${submissionId}`, JSON.stringify(payload), {
-    EX: 3600,
-  });
-
-  const submissionResult = await pushSubmission(payload);
-
-  console.log(submissionResult);
-
-  res.status(200).json({
-    submissionId,
-    message: "Submission received",
-  });
-});
+);
