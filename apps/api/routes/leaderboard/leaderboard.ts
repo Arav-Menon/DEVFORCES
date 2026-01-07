8cb323b1f55abb277188eff7acb3e4dc0c5d294a
Arav menon <aravmenon.ak@gmail.com>
2026-01-07 23:53:57 +0530

    remove leaderboard fiel

--- apps/api/routes/leaderboard/leaderboard.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { middleware } from "../middleware/auth";
-
-export const leaderboardRouter = express();
-
-leaderboardRouter.post("/:contestId", middleware, (req, res) => {
-  const userId = req.id;
-  const contestId = req.params;
-});
