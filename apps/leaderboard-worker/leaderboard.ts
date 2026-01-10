9290bc73e77ea598af86d0a3b1fa7777d8bef651
Arav menon <aravmenon.ak@gmail.com>
2026-01-10 21:54:16 +0530

    feat(leaderboard): setup pull queue and leaderboard on worker

--- apps/leaderboard-worker/leaderboard.ts
+++ apps/leaderboard-worker/leaderboard.ts
@@ -1,5 +1,22 @@
-import { getLeaderBoard } from "@repo/redis-stream/redis-client";
+import {
+  addToLeaderBoard,
+  pullLeaderboardEvent,
+} from "@repo/redis-stream/redis-client";
 
 while (true) {
-  const addLeaderBoard = await getLeaderBoard(contestId);
+  const event = await pullLeaderboardEvent();
+
+  if (!event) {
+    continue;
+  }
+
+  //@ts-ignore
+  const { userId, contestId, score } = event;
+
+  try {
+    const addLeaderBoard = await addToLeaderBoard({ userId, contestId, score });
+    console.dir(addLeaderBoard, { depth: null });
+  } catch (err) {
+    console.error(err, "failed to add to leaderboard event");
+  }
 }
