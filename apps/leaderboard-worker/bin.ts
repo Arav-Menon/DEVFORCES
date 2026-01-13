350ece5e1470750eccfc8be6e46541ba6bfd0c7a
Arav menon <aravmenon.ak@gmail.com>
2026-01-13 23:56:07 +0530

    Feat : add cluster module on leaderboard worker

---
+++ apps/leaderboard-worker/bin.ts
@@ -0,0 +1,18 @@
+import cluster from "cluster";
+import os from "os";
+import { app } from "./server";
+
+const totalCpus = os.cpus().length;
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < totalCpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", () => {
+    cluster.fork();
+  });
+} else {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running at PORT ${process.env.PORT}`);
+  });
+}
