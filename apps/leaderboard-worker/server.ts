5ff5f9a360aeeec1eabaad5fefda1ba9e31a0253
Arav menon <aravmenon.ak@gmail.com>
2026-01-13 23:57:20 +0530

    Del : server port listing

--- apps/leaderboard-worker/server.ts
+++ apps/leaderboard-worker/server.ts
@@ -2,12 +2,8 @@ import { getLeaderBoard } from "@repo/redis-stream/redis-client";
 import express from "express";
 import "dotenv/config";
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/leaderboard", getLeaderBoard);
-
-app.listen(process.env.PORT, () => {
-  console.log(`leaderboard sever is started at ${process.env.PORT}`);
-});
