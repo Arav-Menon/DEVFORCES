acd385ff6b22be7b3f88f26f1d1dfd214c8be7ee
Arav menon <aravmenon.ak@gmail.com>
2026-01-15 22:12:33 +0530

    fix: replace getLeaderBoard with getLeaderBoardRouter in leaderboard API

--- apps/leaderboard-worker/server.ts
+++ apps/leaderboard-worker/server.ts
@@ -1,9 +1,9 @@
-import { getLeaderBoard } from "@repo/redis-stream/redis-client";
 import express from "express";
 import "dotenv/config";
+import { getLeaderBoardRouter } from "./routes/getLeaderboard";
 
 export const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/leaderboard", getLeaderBoard);
+app.use("/api/v1/leaderboard", getLeaderBoardRouter);
