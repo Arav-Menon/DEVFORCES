827d550e33ebd2b18a79dd2759d9434bfdf96fa4
Arav menon <aravmenon.ak@gmail.com>
2026-01-15 22:12:37 +0530

    fix: add connection log to WebSocket server

--- apps/realtime/server.ts
+++ apps/realtime/server.ts
@@ -5,6 +5,7 @@ import { startNotificationSubscriber } from "./broadcast/subscriber";
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws: any) => {
+  console.log("connect to server")
   clients.add(ws);
 
   ws.on("close", () => {
