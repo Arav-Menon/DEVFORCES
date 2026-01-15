fac1dd3cd3a61aa33585192b420723f6b1a1e08f
Arav menon <aravmenon.ak@gmail.com>
2026-01-15 22:12:21 +0530

    fix: correctly extract userId, contestId, and score from leaderboard event

--- apps/leaderboard-worker/leaderboard.ts
+++ apps/leaderboard-worker/leaderboard.ts
@@ -4,14 +4,22 @@ import {
 } from "@repo/redis-stream/redis-client";
 
 while (true) {
-  const event = await pullLeaderboardEvent();
+  const event = (await pullLeaderboardEvent()) as any;
+
+  console.dir(event, {depth : null});
 
   if (!event) {
     continue;
   }
 
-  //@ts-ignore
-  const { userId, contestId, score } = event;
+
+  const stream = event[0];
+  const record = stream?.messages[0];
+
+  
+  const { userId, contestId, score } = record.message;
+
+  console.log("Processing", record.id)
 
   try {
     const addLeaderBoard = await addToLeaderBoard({ userId, contestId, score });
