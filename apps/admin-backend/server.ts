10fbc43a6bf15d417d088726ecac2f029944771e
Arav menon <aravmenon.ak@gmail.com>
2026-01-18 23:11:25 +0530

    feat: Implement admin backend with authentication, user management, and user blocking capabilities.

--- apps/admin-backend/server.ts
+++ apps/admin-backend/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express";
+import "dotenv/config";
+import { AdminAuthrouter } from "./routes/admin-auth/admin.auth";
+import { Adminrouter } from "./routes/admin-auth/admin";
+export const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/admin", AdminAuthrouter);
+app.use("/api/v1/admin", Adminrouter);
+
+app.listen(process.env.PORT);
