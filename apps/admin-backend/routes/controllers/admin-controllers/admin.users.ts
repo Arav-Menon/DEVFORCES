346ca7674668b7801f0f57d8b532b1209077be03
Arav menon <aravmenon.ak@gmail.com>
2026-01-19 19:45:20 +0530

    feat: Add role-based authorization to the user search route

--- apps/admin-backend/routes/controllers/admin-controllers/admin.users.ts
+++ apps/admin-backend/routes/controllers/admin-controllers/admin.users.ts
@@ -1,35 +1,41 @@
 import express from "express";
 import { db } from "@repo/db/db";
 import { middleware } from "../../middleware/auth.middleware";
+import { authorizeRole } from "../../middleware/authorizeRole";
 import "dotenv/config";
 import { adminUserSchema } from "@repo/common/validation";
 
 export const AdminUsersRouter = express();
 
-AdminUsersRouter.get("/search/users", middleware, async (req, res) => {
-  const result = adminUserSchema.safeParse(req.query);
+AdminUsersRouter.get(
+  "/search/users",
+  middleware,
+  authorizeRole(["ADMIN"]),
+  async (req, res) => {
+    const result = adminUserSchema.safeParse(req.query);
 
-  if (!result.success) {
-    return res.status(400).json({
-      error: result.error.flatten().fieldErrors,
+    if (!result.success) {
+      return res.status(400).json({
+        error: result.error.flatten().fieldErrors,
+      });
+    }
+    const username = result.data.username;
+
+    if (!username) return res.status(400).json("username required");
+
+    const findUser = await db.user.findUnique({
+      where: { username },
     });
-  }
-  const username = result.data.username;
 
-  if (!username) return res.status(400).json("username required");
+    if (!findUser) return res.status(404).json("user not found");
 
-  const findUser = await db.user.findUnique({
-    where: { username },
-  });
-
-  if (!findUser) return res.status(404).json("user not found");
-
-  res.json({
-    user: {
-      id: findUser.id,
-      username: findUser.username,
-      email: findUser.email,
-      role: findUser.role,
-    },
-  });
-});
+    res.json({
+      user: {
+        id: findUser.id,
+        username: findUser.username,
+        email: findUser.email,
+        role: findUser.role,
+      },
+    });
+  },
+);
