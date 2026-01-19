c0f0983d816fb8e436243e526f435854a9200d9e
Arav menon <aravmenon.ak@gmail.com>
2026-01-19 19:45:01 +0530

    fix: Correct route imports and ensure consistent API endpoint structure for admin routes

--- apps/admin-backend/server.ts
+++ apps/admin-backend/server.ts
@@ -2,11 +2,22 @@ import express from "express";
 import "dotenv/config";
 import { AdminAuthrouter } from "./routes/admin-auth/admin.auth";
 import { Adminrouter } from "./routes/admin-auth/admin";
+import { AdminUsersRoleRouter } from "./routes/controllers/admin-controllers/admin.updateRole";
+import { AdminUsersBlockRouter } from "./routes/controllers/admin-controllers/admin.users.block";
+import { AdminUsersRouter } from "./routes/controllers/admin-controllers/admin.users";
+import { AdminUsersUnblockRouter } from "./routes/controllers/admin-controllers/admin.users.unblock";
 export const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/admin", AdminAuthrouter);
-app.use("/api/v1/admin", Adminrouter);
+app.use("/api/v1/admin/", AdminAuthrouter);
+app.use("/api/v1/admin/", Adminrouter);
+
+app.use("/api/v1/admin/", AdminUsersBlockRouter);
+app.use("/api/v1/admin/", AdminUsersUnblockRouter);
+
+app.use("/api/v1/admin/", AdminUsersRoleRouter);
+
+app.use("/api/v1/admin/", AdminUsersRouter);
 
 app.listen(process.env.PORT);
