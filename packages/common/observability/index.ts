f4bf0b8788dc6ba7685aad67a636d25872c8f76a
Arav menon <aravmenon.ak@gmail.com>
2026-01-24 23:38:23 +0530

    Refactor: Move primary observability registry to api-observability, introduce primary process usage gauge, and update exports.

--- packages/common/observability/index.ts
+++ packages/common/observability/index.ts
@@ -1,2 +1,3 @@
-export * from "./primary.registry";
-export { register } from "./primary.registry";
+export * from "./api-oberbvability/primary.registry";
+export { register } from "./api-oberbvability/primary.registry";
+export { primaryProcessUsage } from "./api-oberbvability/primary.registry"
