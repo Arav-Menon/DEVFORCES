0e1dd9ff5e87e4517bc7ac2ac8fe2532a00189b7
Arav menon <aravmenon.ak@gmail.com>
2026-02-13 22:25:32 +0530

    feat: add histogram for tracking duration of auth HTTP requests

--- packages/common/observability/api-oberbvability/primary.registry.ts
+++ packages/common/observability/api-oberbvability/primary.registry.ts
@@ -15,3 +15,10 @@ export const primaryProcessUsage = new client.Gauge({
   name: "active_requests",
   help: "Number of active requests",
 });
+
+export const auth_http_duration_ms = new client.Histogram({
+    name: 'aut_http_request_duration_ms',
+    help: 'Duration of auth HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000]
+});
