793977c8db4752476767cee143d176343f5f7bd0
Arav menon <aravmenon.ak@gmail.com>
2026-02-13 22:24:38 +0530

    feat: enhance metrics middleware with duration tracking and refactor function names

--- apps/api/middleware/metrics/metrics.auth.ts
+++ apps/api/middleware/metrics/metrics.auth.ts
@@ -1,10 +1,11 @@
 import {
   authRequestCounter,
   primaryProcessUsage,
+  auth_http_duration_ms,
 } from "@repo/common/observability";
 import type { NextFunction } from "express";
 
-export const authMetricsMiddleware = (
+export const auth_request_counter = (
   req: any,
   res: any,
   next: NextFunction,
@@ -24,7 +25,11 @@ export const authMetricsMiddleware = (
   next();
 };
 
-export const active_requests = (req: any, res: any, next: NextFunction) => {
+export const auth_active_requests_gauge = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
   const startTime = Date.now();
   primaryProcessUsage.inc();
 
@@ -40,5 +45,34 @@ export const active_requests = (req: any, res: any, next: NextFunction) => {
     primaryProcessUsage.dec();
   });
 
+  next();
+};
+
+export const auth_active_request_range = (req: any, res: any, next: NextFunction) => {
+  const startTime = Date.now();
+  primaryProcessUsage.inc();
+
+  res.on("finish", function () {
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
+    // Increment request counter
+    authRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+
+    auth_http_duration_ms.observe(
+      {
+        method: req.method,
+        route: req.route ? req.route.path : req.path,
+        code: res.statusCode,
+      },
+      duration,
+    );
+
+    primaryProcessUsage.dec();
+  });
   next();
 };
