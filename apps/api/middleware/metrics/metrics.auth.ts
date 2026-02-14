85bdbf5e85f796e66dd500f40c95e1f739023e77
Arav menon <aravmenon.ak@gmail.com>
2026-02-15 03:02:16 +0530

    feat: add contest and challenge metrics for request tracking

--- apps/api/middleware/metrics/metrics.auth.ts
+++ apps/api/middleware/metrics/metrics.auth.ts
@@ -2,6 +2,12 @@ import {
   authRequestCounter,
   primaryProcessUsage,
   auth_http_duration_ms,
+  contestRequestCounter,
+  contestActiveRequests,
+  contestHttpDurationMs,
+  challengeRequestCounter,
+  challengeActiveRequests,
+  challengeHttpDurationMs,
 } from "@repo/common/observability";
 import type { NextFunction } from "express";
 
@@ -19,7 +25,6 @@ export const auth_request_counter = (
       method: req.method,
       route: req.route ? req.route.path : req.path,
       statusCode: res.statusCode,
-      response_time: endTime - startTime,
     });
   });
   next();
@@ -48,7 +53,11 @@ export const auth_active_requests_gauge = (
   next();
 };
 
-export const auth_active_request_range = (req: any, res: any, next: NextFunction) => {
+export const auth_active_request_range = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
   const startTime = Date.now();
   primaryProcessUsage.inc();
 
@@ -73,6 +82,146 @@ export const auth_active_request_range = (req: any, res: any, next: NextFunction
     );
 
     primaryProcessUsage.dec();
+  });
+  next();
+};
+
+export const contest_request_counter = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
+  res.on("finish", () => {
+    contestRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+  });
+  next();
+};
+
+export const contest_active_requests_gauge = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
+  const startTime = Date.now();
+  contestActiveRequests.inc();
+
+  res.on("finish", function () {
+    const endTime = Date.now();
+    console.log(`Request took ${endTime - startTime}ms`);
+
+    contestRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+    contestActiveRequests.dec();
+  });
+
+  next();
+};
+
+export const counter_active_request_range = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
+  const startTime = Date.now();
+  contestActiveRequests.inc();
+
+  res.on("finish", function () {
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
+    contestRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+
+    contestHttpDurationMs.observe(
+      {
+        method: req.method,
+        route: req.route ? req.route.path : req.path,
+        code: res.statusCode,
+      },
+      duration,
+    );
+
+    contestActiveRequests.dec();
+  });
+  next();
+};
+
+export const challege_request_counter = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
+  res.on("finish", () => {
+    challengeRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+  });
+  next();
+};
+
+export const challege_active_requests_gauge = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
+  const startTime = Date.now();
+  challengeActiveRequests.inc();
+
+  res.on("finish", function () {
+    const endTime = Date.now();
+    console.log(`Request took ${endTime - startTime}ms`);
+
+    challengeRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+    challengeActiveRequests.dec();
+  });
+
+  next();
+};
+
+export const challege_active_request_range = (
+  req: any,
+  res: any,
+  next: NextFunction,
+) => {
+  const startTime = Date.now();
+  challengeActiveRequests.inc();
+
+  res.on("finish", function () {
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
+    challengeRequestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+
+    challengeHttpDurationMs.observe(
+      {
+        method: req.method,
+        route: req.route ? req.route.path : req.path,
+        code: res.statusCode,
+      },
+      duration,
+    );
+
+    challengeActiveRequests.dec();
   });
   next();
 };
