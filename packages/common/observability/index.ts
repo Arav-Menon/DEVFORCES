364505dd7379525cc8e732be482be5754aae16c7
Arav menon <aravmenon.ak@gmail.com>
2026-02-16 22:07:30 +0530

    fix: update observability exports for user and challenge registries

--- packages/common/observability/index.ts
+++ packages/common/observability/index.ts
@@ -1,4 +1,5 @@
 export { register } from "./register";
-export * from "./api-oberbvability/auth.registry";
+export * from "./api-oberbvability/user.registry";
 export * from "./api-oberbvability/contest.registry";
-export * from "./api-oberbvability/challege.registry"
+export * from "./api-oberbvability/challege.registry";
+export * from "./api-oberbvability/submit.registry";
