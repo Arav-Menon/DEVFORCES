d3d186da236d9675ea2c9f773edfb95564d7d145
Arav menon <aravmenon.ak@gmail.com>
2026-02-19 23:27:18 +0530

    feat: Implement create challenge pages

---
+++ apps/web2/app/(home)/create-challenges/page.tsx
@@ -0,0 +1,9 @@
+import CreateProblemPage from "@/components/createChallengesCompo/createChallenge";
+
+export default function CreateChallenge() {
+    return(
+        <>
+        <CreateProblemPage/>
+        </>
+    )
+}
