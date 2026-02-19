2238d655be891b1300cf9f7eeadab1444176957f
Arav menon <aravmenon.ak@gmail.com>
2026-02-19 23:27:47 +0530

    feat: Implement create contest pages

---
+++ apps/web2/app/(home)/create-contests/page.tsx
@@ -0,0 +1,9 @@
+import CreateContestPage from "@/components/createContestCompo/createContest";
+
+export default function CreateContests() {
+  return (
+    <>
+      <CreateContestPage />
+    </>
+  );
+}
