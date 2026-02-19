0fb45d19b4a233ef96e456ccd0347771d4b0815f
Arav menon <aravmenon.ak@gmail.com>
2026-02-19 23:26:58 +0530

    feat: Implement contest challenge pages

---
+++ apps/web2/app/(home)/contests/[Id]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import ContestDetailPage from "@/components/contests/contest_challenges_component";
+
+export default function ContestChallengePage() {
+  return(
+    <>
+    <ContestDetailPage params={{
+        id: ""
+      }}/>
+    </>
+  )
+}
