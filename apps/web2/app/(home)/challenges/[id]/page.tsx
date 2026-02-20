5bcab4e71137cb7b5f75045ea0c152a5c5bb4ffc
Arav menon <aravmenon.ak@gmail.com>
2026-02-20 23:18:46 +0530

    feat : implement params to get challenge id

--- apps/web2/app/(home)/challenges/[id]/page.tsx
+++ apps/web2/app/(home)/challenges/[id]/page.tsx
@@ -1,14 +1,8 @@
+"use client";
+import { use } from "react";
 import ChallengesPage from "@/components/challenges/challengesCompo";
-import ContestChallengePage from "../../contests/[Id]/page";
 
-export default function Challenges() {
-  return (
-    <>
-      <ChallengesPage
-        params={{
-          id: "",
-        }}
-      />
-    </>
-  );
+export default function Challenges({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params);
+  return <ChallengesPage slug={id} />;
 }
