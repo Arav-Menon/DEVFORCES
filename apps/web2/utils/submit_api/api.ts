86e68ff72469bddc9f466fb4a0150298137141b1
Arav menon <aravmenon.ak@gmail.com>
2026-02-21 23:36:16 +0530

    feat: impelement end to end axios api connction to backend for code submission

---
+++ apps/web2/utils/submit_api/api.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { API_URL } from "../api_url";
+
+const axiosInstence = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstence.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
+
+export const codeSubmission = async (
+  contestId: string,
+  challengeId: string,
+  code: string,
+  language: string,
+) => {
+  const response = await axiosInstence.post(
+    `/api/v1/challenge/submit/${contestId}/${challengeId}`,
+    { code, language },
+  );
+  console.log(response.data.message);
+  return response.data;
+};
