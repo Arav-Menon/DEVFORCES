51675c10dcbc0c42d1f401ac32e1084ef36db9e8
Arav menon <aravmenon.ak@gmail.com>
2026-02-26 22:46:38 +0530

    Feat : add redis cache keys of challenge and contest

---
+++ packages/common/cache-keys/keys.ts
@@ -0,0 +1,5 @@
+export const allContestKey = "contest:all";
+export const contestChallengeKey = (contestId: string) => `contest:${contestId}`;
+export const challengeIdKey = (challengeId: string) =>
+    `challenge:${challengeId}`;
+export const userDataKey = (userId: string) => `user:${userId}`;
