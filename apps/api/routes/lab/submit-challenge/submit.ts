4f1acd5d95a21ab873053ee34d0bb0cc3dd4bfca
Arav menon <aravmenon.ak@gmail.com>
2026-02-26 22:40:55 +0530

    feat: Implement Redis caching to submit  codeIand introduce a `cache-keys` package, and refactor the API

--- apps/api/routes/lab/submit-challenge/submit.ts
+++ apps/api/routes/lab/submit-challenge/submit.ts
@@ -3,7 +3,7 @@ import { getSystemPrompt } from "../../prompt";
 import { getChallengeById } from "./fetchId";
 import { v4 as uuidV4 } from "uuid";
 import { middleware } from "../../../middleware/auth";
-import { client, pushSubmission } from "@repo/redis-stream/redis-client";
+import { pushSubmission } from "@repo/redis-stream/redis-client";
 import { submitLimiter } from "@repo/common/rateLimit";
 import { submit_metrics_middleware } from "../../../middleware/metrics";
 import { db } from "@repo/db/db";
@@ -18,50 +18,53 @@ submitRouter.post(
   submitLimiter,
   async (req, res) => {
     const userId = req.user?.id;
-    const challengeId = req.params.challengeId;
-    const contestId = req.params.contestId;
+    const challengeId = req.params.challengeId as string;
+    const contestId = req.params.contestId as string;
     const { code, language } = req.body;
 
-    if (!challengeId || !code || !language || !userId || !contestId) {
-      return res.status(400).json({
-        message: "Invalid submission",
+    try {
+      if (!challengeId || !code || !language || !userId || !contestId) {
+        return res.status(400).json({
+          message: "Invalid submission",
+        });
+      }
+
+      const submissionId = uuidV4();
+      const challenge = await getChallengeById(challengeId);
+      const systemPrompt = getSystemPrompt(challenge);
+
+      const payload = {
+        submissionId,
+        contestId,
+        userId: userId as string,
+        challengeId,
+        code,
+        language,
+        systemPrompt,
+      };
+
+      await db.submission.create({
+        data: {
+          submissionId: payload.submissionId,
+          userId: payload.userId,
+          challengeId: payload.challengeId,
+          status: "PENDING",
+        },
+      });
+
+      const submissionResult = await pushSubmission(payload);
+
+      console.log(submissionResult);
+
+      res.status(200).json({
+        submissionId,
+        message: "Submission received",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Internal server error",
       });
     }
-
-    const submissionId = uuidV4();
-    const challenge = await getChallengeById(challengeId);
-    const systemPrompt = getSystemPrompt(challenge);
-
-    const payload = {
-      submissionId,
-      contestId,
-      userId: userId as string,
-      challengeId,
-      code,
-      language,
-      systemPrompt,
-    };
-
-    await db.submission.create({
-      data: {
-        submissionId: payload.submissionId,
-        userId: payload.userId,
-        challengeId: payload.challengeId,
-        status: "PENDING",
-      },
-    });
-
-    await client.set(`submission:${submissionId}`, JSON.stringify(payload), {
-      EX: 3600,
-    });
-
-    const submissionResult = await pushSubmission(payload);
-
-    console.log(submissionResult);
-
-    res.status(200).json({
-      submissionId,
-      message: "Submission received",
-    });
   },
 );
