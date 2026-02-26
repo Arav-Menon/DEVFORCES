2674b2d6fb59856ba77ad0a26d66ec33dec858a7
Arav menon <aravmenon.ak@gmail.com>
2026-02-26 22:31:57 +0530

    feat : setup endpoint of delete challenge router of contest challenge

--- apps/api/routes/lab/challenge/delete.challenge.ts
+++ apps/api/routes/lab/challenge/delete.challenge.ts
@@ -1,9 +1,50 @@
 import express from "express";
+import { db } from "@repo/db/db";
+import { middleware } from "../../../middleware/auth";
+import { authorizeRole } from "../../../middleware/authorizeRole";
 
 export const deleteChallengeRouter = express.Router();
 
-deleteChallengeRouter.get("/delete-challenge", (_, res) => {
-    res.json({
-        message : "perfect"
-    })
-})
+deleteChallengeRouter.delete(
+  "/:contestId/challenge/:challengeId",
+  middleware,
+  authorizeRole(["ADMIN", "CREATOR"]),
+  async (req, res) => {
+    const { contestId, challengeId } = req.params;
+
+    if (!contestId || !challengeId) return;
+
+    try {
+      const findContest = await db.contest.findUnique({
+        where: {
+          id: contestId,
+        },
+      });
+
+      const findChallenge = await db.challenge.findUnique({
+        where: { id: challengeId },
+      });
+
+      if (!findContest || !findChallenge) {
+        res.status(404).json({
+          message: "contest or challenge not found",
+        });
+      }
+
+      const removeChallenge = await db.challenge.delete({
+        while: { id: challengeId },
+      });
+
+      res.status(204).json({
+        challenge: removeChallenge.title,
+        message: `challenge deleted succesfully `,
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(500).json({
+        err: err,
+      });
+    }
+  },
+);
