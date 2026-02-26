6e9fd0efc61456fa46c00d3fbfacff5dbfe415ac
Arav menon <aravmenon.ak@gmail.com>
2026-02-26 22:42:30 +0530

    feat: Implement Redis caching to get  user  data introduce a `cache-keys` package, and refactor the API

--- apps/api/routes/user/user.ts
+++ apps/api/routes/user/user.ts
@@ -4,6 +4,8 @@ import { db } from "@repo/db/db";
 import { middleware } from "../../middleware/auth";
 import { user_profile_metrics_middleware } from "../../middleware/metrics";
 import { userProfileDbQueryDurationMs } from "@repo/common/observability";
+import { userDataKey } from "@repo/common/index";
+import { client } from "@repo/redis-stream/redis-client";
 
 export const userRoute = express.Router();
 
@@ -12,8 +14,19 @@ userRoute.get(
   user_profile_metrics_middleware,
   middleware,
   async (req: Request, res: Response) => {
+    const userId = req.id;
     try {
-      const userId = req.id;
+      try {
+        const cachedData = await client.get(userDataKey(userId!));
+        if (cachedData) {
+          return res.status(200).json({
+            user: JSON.parse(cachedData),
+          });
+        }
+      } catch (redisError) {
+        console.log(`Redis cache GET error ${redisError}`);
+      }
+
       const endDbTimer = userProfileDbQueryDurationMs.startTimer({
         operation: "findUnique",
         model: "User",
@@ -23,6 +36,12 @@ userRoute.get(
         select: { username: true, email: true },
       });
       endDbTimer({ success: "true" });
+
+      await client.setEx(
+        userDataKey(userId as string),
+        300,
+        JSON.stringify(user),
+      );
 
       if (!user) return res.status(404).json({ message: "User not found" });
       return res.status(200).json({ user });
