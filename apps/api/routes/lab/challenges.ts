0bac7a3335393a8aeb29b06ba5cc2e38d25dc58d
Arav menon <aravmenon.ak@gmail.com>
2026-02-26 22:28:34 +0530

    feat : Implement Redis caching for  challenge data, introduce cache key management, and refactor challenge deletion logic.

--- apps/api/routes/lab/challenges.ts
+++ apps/api/routes/lab/challenges.ts
@@ -2,8 +2,9 @@ import express from "express";
 import { middleware } from "../../middleware/auth";
 import { db } from "@repo/db/db";
 import { challengeIdFetchDurationMs } from "@repo/common/observability";
-
+import { contestChallengeKey } from "@repo/common/index";
 import { challenge_metrics_middleware } from "../../middleware/metrics";
+import { client } from "@repo/redis-stream/redis-client";
 
 export const challengesRouter = express.Router();
 challengesRouter.use(challenge_metrics_middleware);
@@ -11,6 +12,17 @@ challengesRouter.use(challenge_metrics_middleware);
 challengesRouter.get("/:contestId/challenges", middleware, async (req, res) => {
   const contestId = req.params.contestId;
   try {
+    try {
+      const cacheData = await client.get(contestChallengeKey(contestId));
+      if (cacheData) {
+        return res.status(200).json({
+          challenge: JSON.parse(cacheData),
+        });
+      }
+    } catch (redisError) {
+      console.error(`Redis cache GET error  ${redisError}`);
+    }
+
     const endTime = challengeIdFetchDurationMs.startTimer({
       operation: "findUnique",
       model: "Contest",
@@ -33,6 +45,12 @@ challengesRouter.get("/:contestId/challenges", middleware, async (req, res) => {
         contestId,
       },
     });
+
+    try {
+      await client.setEx(contestChallengeKey(contestId), 300, JSON.stringify(challenges));
+    } catch (redisError) {
+      console.error(`Redis cache SET error ${redisError}`);
+    }
 
     if (challenges.length == 0) {
       endTime({ success: "true" });
