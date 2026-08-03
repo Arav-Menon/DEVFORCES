c60c8a710f254f11fc66e0ed7fc256cb5d454c8d
Arav menon <aravmenon.ak@gmail.com>
2026-08-04 04:55:28 +0530

    fix: remove error object from unauthorized response messages in authorizeRole middleware

--- apps/api/middleware/authorizeRole.ts
+++ apps/api/middleware/authorizeRole.ts
@@ -2,13 +2,11 @@ import type { Request, Response, NextFunction } from "express";
 
 export const authorizeRole = (allowedRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    //@ts-ignore
     if (!req.user) {
-      return res.status(401).json({ message: "Unauthenticated", Error: Error });
+      return res.status(401).json({ message: "Unauthenticated" });
     }
-    //@ts-ignore
     if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Acces denied", Error: Error });
+      return res.status(403).json({ message: "Access denied" });
     }
 
     next();
